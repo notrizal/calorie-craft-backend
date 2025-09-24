2495cb2aa598b824e3799e4d4a21399054bfa032
Rizal Ramadhan <rizalramadhan2214@gmail.com>
2025-09-24 07:04:59 +0700

    memperbaru setting cors untuk domain yang lebih spesifik

--- app.js
+++ app.js
@@ -3,9 +3,21 @@ const cors = require("cors");
 const bmiRoutes = require("./routes/bmiRoutes.js");
 const recipeRoutes = require("./routes/recipeRoutes.js");
 
+const allowedOrigins = [
+    "http://127.0.0.1:5500",
+    "http://localhost:5500",
+    "https://calorie-craft-frontend.netlify.app",
+];
+
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(
+    cors({
+        origin: allowedOrigins,
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        credentials: true,
+    })
+);
 
 // Routing
 app.use("/api/bmi", bmiRoutes);
